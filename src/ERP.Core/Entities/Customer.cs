766b80c7e083e33613df3721dca6328487288e59
= <=>
2025-11-06 11:59:16 -0500

    Add initial project structure with Docker support and core entities
    # Conflicts:
    #	src/ERP.Core/Entities/Customer.cs
    #	src/ERP.Data/Repositories/CustomerRepository.cs

--- src/ERP.Core/Entities/Customer.cs
+++ src/ERP.Core/Entities/Customer.cs
@@ -3,14 +3,22 @@ namespace ERP.Core.Entities;
 
 public class Customer
 {
+    public Customer()
+    {
+        
+    }
+    //Propities 
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string IdDocument { get; set; } = string.Empty;
     public string? Phone { get; set; } 
-    public string Address { get; set; } 
+    public string? Address { get; set; } 
     
-    
+    // vitural collection
     public virtual ICollection<Sale> Sales { get; set; } = [];
+    
+    // Desactive the Customer
+    public bool IsActive { get; set; } = true;
 }
--- src/ERP.Data/Repositories/CustomerRepository.cs
+++ src/ERP.Data/Repositories/CustomerRepository.cs
@@ -1,9 +1,67 @@
+
+
 using ERP.Core.Entities;
-using ERP.Core.Models;
 using ERP.Core.Interfaces;
+using ERP.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace ERP.Data.Repositories;
 
-public class CustomerRepository
+public class CustomerRepository(ApplicationDbContext context) : ICustomerRepository
 {
-    
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<Customer?> GetByIsAsync(int id)
+    {
+        return await _context.Customers
+            .FirstOrDefaultAsync(c => c.Id == id && c.IsActive);
+    }
+
+    public async Task<ResponsePage<Customer>> GetAllAsync(int pageNumber, int pageSize)
+    {
+        var totalCount = await _context.Customers
+            .Where(c => c.IsActive)
+            .CountAsync();
+            
+
+        var items = await _context.Customers
+            .OrderBy(c => c.Id)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        var response = new ResponsePage<Customer>
+        {
+            Items = items,
+            TotalCount = totalCount,
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        };
+        return response;
+    }
+
+    public async Task AddAsync(Customer customer)
+    {
+        await _context.Customers.AddAsync(customer);
+        
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Customer customer)
+    {
+        _context.Customers.Update(customer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+
+        if (customer != null)
+        {
+            customer.IsActive = false;
+
+            await _context.AddRangeAsync();
+        }
+    }
 }
